6eed64e88072431e4f10ecd99758947eedf6d1a5
Shrey0207 <shreytolasaria4297@gmail.com>
2024-08-24 16:52:19 +0530

    cors error

--- index.js
+++ index.js
@@ -2,20 +2,33 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 import cors from "cors";
+
 // Load environment variables from .env file
 dotenv.config();
 
-//db connection
+// DB connection
 import "./db/conn.js";
 
-//routes
+// Routes
 import router from "./router/routes.js";
 
 const port = process.env.PORT;
 
+// Configure CORS
+const allowedOrigins = [
+  "https://shareway-backend.onrender.com",
+  "https://share-way-frontend.vercel.app", // Add your frontend URL here
+];
+
 app.use(
   cors({
-    origin: "http://localhost:3000", // Specify your client origin here
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true, // Allow credentials
   })
 );
@@ -23,16 +36,17 @@ app.use(
 app.options(
   "*",
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Adjust the origin as needed
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
 app.use(express.json());
 app.use(router);
 app.listen(port, () => {
